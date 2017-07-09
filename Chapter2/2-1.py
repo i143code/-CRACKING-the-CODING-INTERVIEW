80cfa37c547bf68d841341e558b2dc253dc479c2
ashish_mercer <ashish.ranjan@mercer.com>
2017-07-09 15:18:03 -0400

    Chatpter 2.1

---
+++ Chapter2/2-1.py
@@ -0,0 +1,54 @@
+class Node(object):
+  def __init__(self,data):
+    self.data = data
+    self.nextNode = None  
+
+class LinkedList(object):
+  head = None
+  tail = None
+  
+  # insert at end
+  def InsertAtEnd(self,data):
+    new_node = Node(data)
+    if self.head is None:
+      self.head = self.tail = new_node
+    else:
+      self.tail.nextNode = new_node
+      self.tail = new_node
+  
+  # print 
+  def traverse(self):
+    current = self.head
+    
+    while current != None:
+      print(current.data)
+      current = current.nextNode
+  
+  # 2.1 remove duplicates
+  
+  def deleteDups(self):
+    if self.head  != None:
+      current = self.head
+      seen = {current.data :True}
+      while current.nextNode != None:
+        # is data seen in hash 
+        if current.nextNode.data in seen:
+          current.nextNode = current.nextNode.nextNode
+        
+        # else data mot seen in hash
+        else:
+          seen[current.nextNode.data] = True
+          current = current.nextNode
+      
+
+
+      
+a = LinkedList()
+a.InsertAtEnd(1)
+a.InsertAtEnd(1)
+a.InsertAtEnd(3)
+a.InsertAtEnd(4)
+print(a.traverse())
+# a.deleteDups()
+a.deleteDups()
+print(a.traverse())
