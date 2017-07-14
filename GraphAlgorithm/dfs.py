2af9affc60bcc2cdd487398cb767316c81d855aa
ashish_mercer <ashish.ranjan@mercer.com>
2017-07-13 23:09:58 -0400

    dfs

---
+++ GraphAlgorithm/dfs.py
@@ -0,0 +1,36 @@
+class Node(object):
+    def __init__(self,data):
+        self.data = data
+        self.adjacent = []
+        self.visited = False
+
+class DeapthFirstSearch(object):
+    # recursive
+    def dfs(self,node):
+        node.visited = True
+        print(node.data)
+
+        for n in node.adjacent:
+            if not n.visited:
+                self.dfs(n)
+
+
+
+
+
+node1 = Node("A")
+node2 = Node("B")
+node3 = Node("C")
+node4 = Node("D")
+node5 = Node("E")
+node6 = Node("F")
+
+node1.adjacent.append(node2)
+node1.adjacent.append(node5)
+node1.adjacent.append(node4)
+node2.adjacent.append(node3)
+node2.adjacent.append(node6)
+
+dfs = DeapthFirstSearch()
+dfs.dfs(node1)
+
